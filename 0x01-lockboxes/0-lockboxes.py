37816687ca3740c9f69c59e799d456304931f0d9
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-14 22:34:02 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,14 +3,14 @@
 
 
 def canUnlockAll(boxes):
-    """ Unlocking the boxes """
+    """ Doc """
     if not boxes or len(boxes) == 0:
         return False
-    k = [0]
-    for key in k:
+    keys = [0]
+    for key in keys:
         for new_key in boxes[key]:
-            if new_key not in k and new_key < len(boxes):
+            if new_key not in keys and new_key < len(boxes):
                 keys.append(new_key)
-    if len(k) == len(boxes):
+    if len(keys) == len(boxes):
         return True
     return False
