6aeeac6073fc0e6ed3fca040db5588d0ccb98444
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-14 22:40:44 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,15 +2,15 @@
 """ Function for lock-boxes """
 
 
-def can_unlock_all(boxes):
-    """ Doc """
+def canUnlockAll(boxes):
+    """ Docs """
     if not boxes or len(boxes) == 0:
         return False
-    reachable_boxes = [0]
-    for box in reachable_boxes:
-        for new_box in boxes[box]:
-            if new_box not in reachable_boxes and new_box < len(boxes):
-                reachable_boxes.append(new_box)
-    if len(reachable_boxes) == len(boxes):
+    kys = [0]
+    for key in kys:
+        for n_key in boxes[key]:
+            if n_key not in kys and n_key < len(boxes):
+                kys.append(n_key)
+    if len(kys) == len(boxes):
         return True
     return False
