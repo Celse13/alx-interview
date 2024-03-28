d0f01aadd7a4c2c292fbfb8641ea8251093ff109
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-28 22:00:25 +0200

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,38 +1,44 @@
 #!/usr/bin/python3
-""" Parsing the log """
+"""parsing logs """
 
 
-def print_stats(status, file_size):
-    """  print the stats"""
-    print(f"File size: {file_size}")
+def print_stats(status, size):
+    """Compute metrics"""
+    print(f"File size: {size}")
     for key, value in sorted(status.items()):
         if value != 0:
-            print("{:s}: {:d}".format(key, value))
+            print('{}: {}'.format(key, value))
 
 
 count = 0
 file_size = 0
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
-
+status_info = {"200": 0, "301": 0, "400": 0, "401": 0,
+              "403": 0, "404": 0, "405": 0, "500": 0}
 
 try:
-    while 1:
+    while True:
         try:
             get_line = input()
+            line_content = get_line.strip().split()
+
             if not get_line:
                 break
-            line_content = get_line.strip().split()
+
             if len(line_content) > 2:
-                file_size += int(line_content[-1])
-                status_code = line_content[-2]
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
+                temp = int(line_content[-1])
+                stat_code = line_content[-2]
+
+                if stat_code in status_info:
+                    status_info[stat_code] += 1
+
+                file_size += temp
                 count += 1
+
                 if count == 10:
-                    print_stats(status_codes, file_size)
+                    print_stats(status_info, file_size)
                     count = 0
-        except (EOFError, KeyboardInterrupt):
+
+        except (KeyboardInterrupt, EOFError):
             break
 finally:
-    print_stats(status_codes, file_size)
+    print_stats(status_info, file_size)
