f35a7585a54612787a0913bd6b54b94d51da1f53
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-04-11 21:04:06 +0200

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python3
-"""N queens"""
 import sys
 
 
-def is_safe(board, row, col):
-    """Check if a queen can be placed on board[row][col]"""
-    for q in range(col):
-        if board[q] == row or \
-                board[q] - q == row - col or \
-                board[q] + q == row + col:
+def safe(b, r, c):
+    """Check if a queen can be placed at row r and column c
+        without attacking any other queen
+    """
+    for q in range(c):
+        if b[q] == r or b[q] - q == r - c or b[q] + q == r + c:
             return False
     return True
 
 
-def nqueens(size):
-    def solve_NQueen(board, col):
-        """Solve N queens"""
-        j = len(board)
-        if col == j:
-            print(str([[q, board[q]] for q in range(j)]))
-            return
-        for row in range(j):
-            if is_safe(board, row, col):
-                board[col] = row
-                solve_NQueen(board, col + 1)
+def solve(b, c):
+    """Solve the n-queens problem using backtracking"""
+    s = len(b)
+    if c == s:
+        print(str([[q, b[q]] for q in range(s)]))
+        return
+    for r in range(s):
+        if safe(b, r, c):
+            b[c] = r
+            solve(b, c + 1)
 
-    brd = [-1 for _ in range(size)]
-    solve_NQueen(brd, 0)
+
+def queens(n):
+    b = [-1 for _ in range(n)]
+    solve(b, 0)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
+        sys.exit(1)
     n = sys.argv[1]
     if not n.isdigit():
         print("N must be a number")
-        exit(1)
+        sys.exit(1)
     if int(n) < 4:
         print("N must be at least 4")
-        exit(1)
-    nqueens(int(n))
+        sys.exit(1)
+    queens(int(n))
