5d6d8a6b97703528ced6795319fe513f3765614e
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-04-18 23:03:38 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,23 @@
 #!/usr/bin/node
-const request = require('request');
-const apiUrl = `https://swapi.dev/api/films/${process.argv[2]}`;
+const req = require('request');
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
 const getChar = (url) => new Promise((resolve, reject) =>
-  request(url, (error, response, charBody) => {
-    if (error || response.statusCode !== 200) {
-      reject(error || `Response status: ${response.statusCode}`);
-    } else {
-      resolve(JSON.parse(charBody).name);
-    }
-  })
+  req(url, (error, response, charBody) =>
+    error || response.statusCode !== 200
+      ? reject(error || response.statusCode)
+      : resolve(JSON.parse(charBody).name)
+  )
 );
 
-request(apiUrl, async (err, res, body) => {
+req(apiUrl, async (err, res, body) => {
   if (!err && res.statusCode === 200) {
     const data = JSON.parse(body);
-    try {
-      const chars = await Promise.all(data.characters.map(getChar));
-      chars.forEach((charName) => {
-        console.log(charName);
-      });
-    } catch (error) {
-      console.error(`Error getting character names: ${error}`);
-    }
+    const chars = await Promise.all(data.characters.map(getChar));
+    chars.forEach((charName) => {
+      console.log(charName);
+    });
   } else {
-    console.error(err || `Response status: ${res.statusCode}`);
+    console.error(err || res.statusCode);
   }
 });
