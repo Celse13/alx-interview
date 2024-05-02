e44a261656a01d332f168ef01dc97efd60a3609b
celse13 <rugiracelse13@gmail.com>
2024-05-02 21:04:48 +0200

    Adding a challenge of making a coin

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def makeChange(coins, total):
+    '''Return the minimum number of coins needed to
+        make change for the total
+    '''
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], 1 + dp[i - coin])
+
+    return dp[total] if dp[total] <= total else -1
