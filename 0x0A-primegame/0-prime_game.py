36cb5c323ea0e765b05b18e29b9bc357dd101262
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-05-16 21:18:19 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,20 +1,61 @@
 #!/usr/bin/python3
-"""checks who wins the game"""
+"""Prime Game
+"""
 
 
-def isWinner(rounds, numbers):
-    """Prime Game"""
-    if not numbers or rounds < 1:
+def is_prime(num):
+    """
+    Check if a number is prime.
+
+    Args:
+        num (int): The number to check.
+
+    Returns:
+        bool: True if the number is prime, False otherwise.
+    """
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def isWinner(x, nums):
+    """Determine the winner of each round in the prime game.
+
+    Args:
+        x (int): The number of rounds.
+        nums (list): A list of integers representing the ranges for each round.
+
+    Returns:
+        str or None: The name of the player that won the most rounds.
+        If the winner cannot be determined, returns None.
+    """
+    # Function to find the next prime number in the set
+    def find_next_prime(current, nums):
+        for i in range(current + 1, len(nums)):
+            if is_prime(nums[i]):
+                return nums[i]
+        return -1
+
+    # Main game logic
+    winners = {'Maria': 0, 'Ben': 0}
+
+    for n in nums:
+        # Counting the number of primes in the given range
+        primes_count = sum(1 for num in range(1, n + 1) if is_prime(num))
+
+        # If the number of primes is even, Ben wins. Otherwise, Maria wins.
+        if primes_count % 2 == 0:
+            winners['Ben'] += 1
+        else:
+            winners['Maria'] += 1
+
+    # Determining the winner with the most wins
+    if winners['Maria'] == winners['Ben']:
         return None
-    max_num = max(numbers)
-    prime_flags = [True for _ in range(max(max_num + 1, 2))]
-    prime_flags[0] = prime_flags[1] = False
-    for index in range(2, int(max_num ** 0.5) + 1):
-        if prime_flags[index]:
-            for j in range(index * index, max_num + 1, index):
-                prime_flags[j] = False
-    prime_numbers = [i for i, is_prime in enumerate(prime_flags) if is_prime]
-    victories = 0
-    for num in numbers:
-        victories += sum(prime <= num for prime in prime_numbers) % 2 == 1
-    return "Maria" if victories * 2 == len(numbers) else "Ben"
+    elif winners['Maria'] > winners['Ben']:
+        return 'Maria'
+    else:
+        return 'Ben'
