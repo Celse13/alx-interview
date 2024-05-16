2a7d85c72286071b69c26c488f0b536b9d5bcfa4
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-05-16 21:14:14 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,32 +2,19 @@
 """checks who wins the game"""
 
 
-def isWinner(x, nums):
-    """checks who wins the game"""
-    def generate_primes(n):
-        primes = [True for _ in range(n+1)]
-        p = 2
-        while p * p <= n:
-            if primes[p] == True:
-                for i in range(p * p, n+1, p):
-                    primes[i] = False
-            p += 1
-        return primes
-
-    Maria_wins = 0
-    Ben_wins = 0
-
-    for n in nums:
-        primes = generate_primes(n)
-        prime_count = sum(primes) - 2
-        if prime_count % 2 == 0:
-            Ben_wins += 1
-        else:
-            Maria_wins += 1
-
-    if Maria_wins > Ben_wins:
-        return "Maria"
-    elif Ben_wins > Maria_wins:
-        return "Ben"
-    else:
+def isWinner(rounds, numbers):
+    """Prime Game"""
+    if not numbers or rounds < 1:
         return None
+    max_num = max(numbers)
+    prime_flags = [True for _ in range(max(max_num + 1, 2))]
+    prime_flags[0] = prime_flags[1] = False
+    for index in range(2, int(max_num ** 0.5) + 1):
+        if prime_flags[index]:
+            for j in range(index * index, max_num + 1, index):
+                prime_flags[j] = False
+    prime_numbers = [i for i, is_prime in enumerate(prime_flags) if is_prime]
+    victories = 0
+    for num in numbers:
+        victories += sum(prime <= num for prime in prime_numbers) % 2 == 1
+    return "Maria" if victories * 2 == len(numbers) else "Ben"
